6231cd78c05af93c5339ad52a493c7886ca7ee8e
jthcast <jthcast@gmail.com>
2021-01-13 02:59:56 +0900

    Add useObserveer for infiniteScroll

---
+++ customHooks/useObserver.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+
+interface ObserverProps {
+  callback: (
+    entry: IntersectionObserverEntry,
+    observer: IntersectionObserver
+  ) => void;
+  root?: Element | null;
+  rootMargin?: string;
+  threshold?: number;
+}
+
+const useObserver = ({
+  callback,
+  root,
+  rootMargin,
+  threshold = 0,
+}: ObserverProps): React.Dispatch<
+  React.SetStateAction<Element[] | undefined>
+> => {
+  const [observerEntry, setObserverEntry] = useState<Array<Element>>();
+
+  useEffect(() => {
+    if (observerEntry) {
+      const observerObj = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach((entry) => {
+            callback(entry, observer);
+          });
+        },
+        { root, rootMargin, threshold }
+      );
+
+      observerEntry.forEach((element: Element) => {
+        observerObj.observe(element);
+      });
+
+      return () => observerObj.disconnect();
+    }
+
+    return () => null;
+  }, [callback, observerEntry, root, rootMargin, threshold]);
+
+  return setObserverEntry;
+};
+
+export default useObserver;
