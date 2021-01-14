8bb8a5a37fd2e2be5fa67b6d8ad6887e7a3f9169
jthcast <jthcast@gmail.com>
2021-01-14 17:41:35 +0900

    Fix global css

---
+++ styles/global-css.ts
@@ -0,0 +1,33 @@
+const globalCss = {
+  breakpoint: {
+    mobile: `17.5rem`,
+    tablet: `40.875rem`,
+    desktop: `64rem`,
+  },
+  common: {
+    maxWidth: `66.667rem`,
+    maxWidthHeader: `100%`,
+    fontBold: `600`,
+    fontNormal: `400`,
+    borderRadius: `0.5rem`,
+    fontSize: `18px`,
+  },
+  color: {
+    backgroundColor: `var(--background-base)`,
+    backgroundColorOpacity: `var(--background-base-opacity)`,
+    backgroundColorDownOpacity: `var(--background-down-opacity)`,
+    borderColor: `var(--border-base)`,
+    groupColor: `var(--group-base)`,
+    color: `var(--color-base)`,
+    colorDown: `var(--color-down)`,
+    primaryBrandColor: `var(--primary-brand-base)`,
+    secondaryBrandColor: `var(--secondary-brand-base)`,
+    backgroundCode: `var(--background-code-base)`,
+    colorCode: `var(--color-code-base)`,
+    black: `var(--black)`,
+    white: `var(--white)`,
+    danger: `var(--danger)`,
+  },
+};
+
+export default globalCss;
