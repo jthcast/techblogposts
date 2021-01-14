bb4cdbcd3511c6b2890be4ed9ad358d2bc955ee9
jthcast <jthcast@gmail.com>
2021-01-14 18:32:57 +0900

    Fix css

--- components/molecules/ScrollButton.tsx
+++ components/molecules/ScrollButton.tsx
@@ -1,4 +1,6 @@
+import { css, cx } from '@emotion/css';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
+import globalCss from '../../styles/global-css';
 
 interface ScrollButtonProps {
   ariaLabel?: string;
@@ -63,8 +65,6 @@ const ScrollButton = ({
     return () => {
       window.removeEventListener('scroll', scrollHandling);
     };
-
-    return () => null;
   }, [scrollHandling]);
 
   return (
@@ -72,9 +72,12 @@ const ScrollButton = ({
       id={id}
       tabIndex={tabIndex}
       aria-label={ariaLabel}
-      className={`jth-scrollButton${ghost ? ` jth-scrollButton-ghost` : ``}${scrollState ? ` jth-scrollButton-on` : ``
-        }${isTopState ? ` jth-scrollButton-top` : ``}${className ? ` ${className}` : ``
-        }`}
+      className={cx(
+        { [cssScrollButton]: true },
+        { [cssGhost]: ghost },
+        { [cssShow]: scrollState },
+        { [className]: true },
+      )}
       onClick={onClick || undefined}
     >
       {children || null}
@@ -83,3 +86,38 @@ const ScrollButton = ({
 };
 
 export default ScrollButton;
+
+const cssScrollButton = css`
+  height: 3.5rem;
+  width: 3.5rem;
+  background-color: ${globalCss.color.primaryBrandColor};
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  transition: 0.2s ease;
+  // transform: translateY(100%); //TODO causing bug
+  visibility: hidden;
+  border: none;
+
+  svg {
+    color: ${globalCss.color.white};
+    font-size: 2.5rem;
+    margin: 0 0.6rem 0 0.6rem;
+  }
+`;
+
+const cssGhost = css`
+  background-color: transparent;
+
+  svg {
+    color: ${globalCss.color.color};
+  }
+`;
+
+const cssShow = css`
+  opacity: 1;
+  visibility: visible;
+  transform: translateY(0);
+`;
