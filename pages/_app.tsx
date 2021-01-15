badc625f1d6612851586c1363871aa287621a21d
jthcast <jthcast@gmail.com>
2021-01-15 22:24:21 +0900

    Fix add infiniteScrollProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import { AppProps } from 'next/app'
 import '../styles/normalize.css'
 import '../styles'
 import { ThemeProvider } from '../context/ThemeContext'
+import { InfiniteScrollProvider } from '../context/InfiniteScrollContext'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <InfiniteScrollProvider>
+        <Component {...pageProps} />
+      </InfiniteScrollProvider>
     </ThemeProvider>
   )
 }
