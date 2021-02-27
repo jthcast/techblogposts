31aca128a1f5c7d86899e759d0267c6417190234
jthcast <jthcast@gmail.com>
2021-02-28 03:55:37 +0900

    Fix shake issue when menulist open

--- components/organisms/MenuList.tsx
+++ components/organisms/MenuList.tsx
@@ -180,12 +180,12 @@ const cssMenuListItems = (menuState: boolean) => css`
     display: flex;
     align-items: center;
     flex-direction: column;
+    transition: transform 830ms cubic-bezier(0.19, 1, 0.22, 1);
+    transform: translateY(-15%);
+    ${menuState && 'transform: translateY(0)'};
 
     li {
       margin-bottom: 1rem;
-      transition: transform 830ms cubic-bezier(0.19, 1, 0.22, 1);
-      transform: translateY(-30%);
-      ${menuState && 'transform: translateY(0)'};
 
       &:nth-last-child(1) {
         margin-bottom: 0;
