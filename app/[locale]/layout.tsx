d4a2c1690916604c4c7e0402a22c7a2c7c3d32d2
jthcast <jthcast@gmail.com>
2023-12-06 03:07:11 +0900

    fix: ThemeProvider folder structure

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import ReactQueryClientProvider from '@/app/providers/ReactQueryClientProvider/ReactQueryClientProvider'
+import ThemeProvider from '@/app/providers/ThemeProvider/ThemeProvider'
 import { locales } from '@/libs/i18n/i18n'
 import { notFound } from 'next/navigation'
 import { ReactNode } from 'react'
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang={locale}>
       <body>
+        <ThemeProvider />
         <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
       </body>
     </html>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import ThemeProvider from '@/app/providers/ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,10 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider />
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
