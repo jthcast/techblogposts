c44bfd5d309c5a48eeb6ba826cfff21235a5d85c
jthcast <jthcast@gmail.com>
2024-01-01 20:38:30 +0900

    feat: useDebounce

---
+++ hooks/useDebounce/useDebounce.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from 'react'
+
+interface UseDebounceProps {
+  value: string
+  delay?: number
+}
+
+export const useDebounce = ({ value, delay = 500 }: UseDebounceProps) => {
+  const [debouncedValue, setDebouncedValue] = useState('')
+  const [isDebouncing, setIsDebouncing] = useState(false)
+  const timerRef = useRef<NodeJS.Timeout>()
+
+  useEffect(() => {
+    setIsDebouncing(true)
+    timerRef.current = setTimeout(() => {
+      setDebouncedValue(value)
+      setIsDebouncing(false)
+    }, delay)
+
+    return () => {
+      clearTimeout(timerRef.current)
+      setIsDebouncing(false)
+    }
+  }, [value, delay])
+
+  return { value: debouncedValue, isDebouncing }
+}
