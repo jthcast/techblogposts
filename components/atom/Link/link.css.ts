b840d1625fb5a7a24b95a26d55ad62da6cbca1eb
jthcast <jthcast@gmail.com>
2024-01-03 01:25:30 +0900

    feat: Link

---
+++ components/atom/Link/link.css.ts
@@ -0,0 +1,56 @@
+import { vars } from '@/app/app.css'
+import { RecipeVariants, recipe } from '@vanilla-extract/recipes'
+import { typography } from '@/providers/ThemeProvider/typography'
+
+export const link = recipe({
+  base: {
+    cursor: 'pointer',
+    transition: 'all 150ms ease-in',
+    textDecoration: 'none',
+    ...typography.callout.regular,
+
+    ':active': {
+      scale: 0.97,
+    },
+  },
+  variants: {
+    color: {
+      primary: {
+        color: vars.colors.labels.primary,
+      },
+      secondary: {
+        color: vars.colors.labels.secondary,
+      },
+      tertiary: {
+        color: vars.colors.labels.tertiary,
+      },
+      accentPrimary: {
+        color: vars.colors.accents.primary,
+
+        ':hover': {
+          color: vars.colors.accents.primary_a90,
+        },
+        ':active': {
+          color: vars.colors.accents.primary_a90,
+        },
+      },
+    },
+    isUnderline: {
+      true: {
+        textDecoration: 'underline',
+      },
+    },
+    isButton: {
+      true: {
+        textDecoration: 'underline',
+        borderRadius: 6,
+        border: 0,
+      },
+    },
+  },
+  defaultVariants: {
+    color: 'primary',
+  },
+})
+
+export type LinkVariants = RecipeVariants<typeof link>
