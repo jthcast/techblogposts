f7ed987cef718103d86aa091fa4ca1a2ab16c6c8
jthcast <jthcast@gmail.com>
2024-01-04 03:40:37 +0900

    fix: layout location

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import ReactQueryClientProvider from '@/providers/ReactQueryClientProvider/ReactQueryClientProvider'
 import ThemeProvider from '@/providers/ThemeProvider/ThemeProvider'
 import { palette } from '@/providers/ThemeProvider/palette'
-import { locales } from '@/libs/i18n/i18n'
-import { notFound } from 'next/navigation'
 import { ReactNode } from 'react'
 import { NextIntlClientProvider, useMessages, useTranslations } from 'next-intl'
 import { Noto_Sans_KR } from 'next/font/google'
 import * as Header from '@/components/atom/Header/Header'
 import { Metadata, Viewport } from 'next'
 import { routes } from '@/constants/routes'
-import * as styles from '@/app/[locale]/layout.css'
+import * as styles from '@/app/layout.css'
 import { SearchCommandDialog } from '@/app/[locale]/components/SearchCommandDialog/SearchCommandDialog'
 import { MenuSheet } from '@/app/[locale]/components/MenuSheet/MenuSheet'
 import { Link } from '@/components/atom/Link/Link'
@@ -55,8 +53,6 @@ export default function RootLayout({
   children,
   params: { locale },
 }: RootLayoutProps) {
-  if (!locales.includes(locale)) notFound()
-
   const messages = useMessages()
   const t = useTranslations()
 
