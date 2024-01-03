f33d09a2c9b71f172cc06aba6a1f42751a1a5147
jthcast <jthcast@gmail.com>
2024-01-04 03:40:52 +0900

    feat: not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+import { Link } from '@/components/atom/Link/Link'
+import { useTranslations } from 'next-intl'
+import * as styles from '@/app/not-found.css'
+import { routes } from '@/constants/routes'
+
+export default function NotFoundPage() {
+  const t = useTranslations()
+
+  return (
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <h1 data-content="404" className={styles.title}>
+          {t('notFoundPage.title')}
+        </h1>
+        <p className={styles.description}>{t('notFoundPage.description')}</p>
+        <Link href={routes.root} isButton color="accentPrimary">
+          {t('notFoundPage.homeButton')}
+        </Link>
+      </section>
+    </main>
+  )
+}
