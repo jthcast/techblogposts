c714cbcba7739a5f1ae640acf73b82a6c8a60f02
jthcast <jthcast@gmail.com>
2024-01-20 20:06:54 +0900

    fix: remove session provider

--- app/[locale]/components/Menu/Menu.tsx
+++ app/[locale]/components/Menu/Menu.tsx
@@ -1,10 +1,5 @@
 import { MenuSheet } from '@/app/[locale]/components/MenuSheet/MenuSheet'
-import { SessionProvider } from '@/providers/SessionProvider/SessionProvider'
 
 export async function Menu() {
-  return (
-    <SessionProvider>
-      <MenuSheet />
-    </SessionProvider>
-  )
+  return <MenuSheet />
 }
